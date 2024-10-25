a2242b2dcc79e49d3bdb9f45f7fa93b1a5c789a5
unknown <sharksoareskosvoski@gmail.com>
2024-10-24 23:43:00 -0400

    Criando metodo listar

--- src/br/com/projeto/dao/VendasDAO.java
+++ src/br/com/projeto/dao/VendasDAO.java
@@ -5,11 +5,14 @@
 package br.com.projeto.dao;
 
 import br.com.projeto.jbdc.ConnectionFactory;
+import br.com.projeto.model.Clientes;
 import br.com.projeto.model.Vendas;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 /**
@@ -70,6 +73,47 @@ public class VendasDAO {
             throw new RuntimeException(e);
         }
         }
+    //metodo que filtra venda por data
+    public List<Vendas> listarVendasPorPeriodo(String data_inicio, String data_fim) {
+
+        try {
+            //Cliando Lista
+            List<Vendas> lista = new ArrayList<>();
+
+            // Comando SQL
+            String sql = "select v.id, v.data_venda, c.nome, v.total_venda, v.observacoes from tb_vendas as v "
+                    + "inner join tb_clientes as c on(v.clientes_id = c.id) where v.data_venda BETWEEN? AND?";
+            
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, data_inicio);
+            stmt.setString(2, data_fim);
+            
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Vendas obj = new Vendas();
+                Clientes c = new Clientes();
+                
+                obj.setId(rs.getInt("v.id"));
+                obj.setData_venda(rs.getString("v.data_venda"));
+                c.setNome(rs.getString("c.nome"));
+                obj.setTotal_venda(rs.getDouble("v.total_venda"));
+                obj.setObs(rs.getString("v.observacoes"));
+                
+                obj.setCliente(c);
+
+                lista.add(obj);
+
+            }
+
+            return lista;
+
+        } catch (SQLException erro) {
+
+            JOptionPane.showMessageDialog(null, "Erro:" + erro);
+            return null;
+        }
+    }
 
 
     
